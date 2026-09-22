867ee08878fb33d3f4447b34b99cb3fa4c0bc3b2
a24kimgalgal <a24kimgalgal@inspedralbes.cat>
2026-09-22 09:18:47 +0200

    feat: port actualitzat

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const { v4: uuidv4 } = require('uuid');
 const path = require('path');
 
 const app = express();
-const port = 3022;
+const port = Number(process.argv[2]) || 40500; 
 
 const preguntes = require('./preguntes.json');
 const respostes = require('./respostes.json');
